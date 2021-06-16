6105c9cb8536c4f84dee07d05c2c91c8d4d5833c
itchaiyaphum <39511454+itchaiyaphum@users.noreply.github.com>
2021-06-16 09:49:36 +0700

    add headadvisor/advisorgroup completed

--- application/views/headadvisor/menu.php
+++ application/views/headadvisor/menu.php
@@ -9,6 +9,9 @@
 	<li class="<?php echo $this->helper_lib->getActiveMenu('users');?>">
 		<a href="<?php echo base_url('headadvisor/users'); ?>"><span class="uk-icon-pie-chart"></span> จัดการข้อมูลครูที่ปรึกษา</a>
 	</li>
+	<li class="<?php echo $this->helper_lib->getActiveMenu('advisorgroup');?>">
+		<a href="<?php echo base_url('headadvisor/advisorgroup'); ?>"><span class="uk-icon-pie-chart"></span> จัดการครูที่ปรึกษาประจำกลุ่ม</a>
+	</li>
 	<li class="<?php echo $this->helper_lib->getActiveMenu('approving');?>">
 		<a href="<?php echo base_url('headadvisor/approving'); ?>"><span class="uk-icon-pie-chart"></span> อนุมัติการส่งข้อมูลกิจกรรมโฮมรูม</a>
 	</li>
